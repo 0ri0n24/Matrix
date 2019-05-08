bd1717982d9e0890a2925471d427cbaa591b88ab
guleri <abhishekguleri24@gmail.com>
2019-05-08 23:52:36 +0530

    choose and adding matirx and various methods added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,46 @@
 package com.company;
+
 import java.util.Scanner;
+
 public class Main {
-private Scanner in = new Scanner(System.in);
+    private Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Welcome to matrix calculator");
         Main use = new Main();
         use.choose();
-        use.parameters();
     }
-     private void choose(){
-         System.out.println("choose what you want to do : ");
-         System.out.println(" 1. Addition" +
-                            "\n 2. Subtraction " +
-                            "\n 3. Multiplication" +
-                            "");
 
-     }
+    private void choose() {
+        System.out.println("choose what you want to do : ");
+        System.out.println(" 1. Addition" +
+                "\n 2. Subtraction " +
+                "\n 3. Multiplication" +
+                "");
+        Switch();
+    }
 
-     private void parameters() {
+    private void Switch() {
+        System.out.println("enter the the number:");
+        int num = in.nextInt();
+        switch (num) {
+            case 1: {
+                addition();
+            }
+            case 2:{
+                subtraction();
+            }
+            case 3:{
+                multiplication();
+            }
+            case 4:{
+                scalerMultiplication();
+            }
+        }
+
+    }
+
+    private void parameters() {
         int row, col;
         System.out.println("enter the numbers of rows and columns in the matrix");
         row = in.nextInt();
@@ -30,5 +53,15 @@ private Scanner in = new Scanner(System.in);
             }
         }
     }
+
+    private void addition() {
+        System.out.println("enter values in first matrix");
+        parameters();
+        System.out.println("enter values in second matrix");
+        parameters();
     }
+    private void subtraction(){}
+    private  void multiplication (){}
+    private void scalerMultiplication(){}
+}
 
