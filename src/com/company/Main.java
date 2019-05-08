ced1fcc06cb4bcf14e7062365350d1374a1ffa67
guleri <abhishekguleri24@gmail.com>
2019-05-08 23:37:52 +0530

    enter numbers to matrix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,14 @@
 package com.company;
 import java.util.Scanner;
 public class Main {
-Scanner in = new Scanner(System.in);
+private Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Welcome to matrix calculator");
         Main use = new Main();
         use.choose();
         use.parameters();
     }
-     public void choose(){
+     private void choose(){
          System.out.println("choose what you want to do : ");
          System.out.println(" 1. Addition" +
                             "\n 2. Subtraction " +
@@ -16,15 +16,19 @@ Scanner in = new Scanner(System.in);
                             "");
 
      }
-     public void parameters(){
 
-         System.out.println("enter the numbers of rows and columns in the matrix");
-         int row,col;
-         row = in.nextInt();
-         col=in.nextInt();
-         System.out.println(" you enter the number of rows = "+ row + " and columns = "+ col);
+     private void parameters() {
+        int row, col;
+        System.out.println("enter the numbers of rows and columns in the matrix");
+        row = in.nextInt();
+        col = in.nextInt();
+        int[][] mat = new int[row][col];
+        System.out.println("enter values:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                mat[i][j] = in.nextInt();
+            }
+        }
+    }
+    }
 
-
-     }
-
-}
