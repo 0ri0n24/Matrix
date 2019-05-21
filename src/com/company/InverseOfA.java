eebef4cdcc318c9e8e6a02f2d82f1a7a587ddb65
guleri <abhishekguleri24@gmail.com>
2019-05-21 23:51:29 +0530

    transpose added

--- src/com/company/InverseOfA.java
+++ src/com/company/InverseOfA.java
@@ -1,13 +1,36 @@
 package com.company;
 import java.util.Scanner;
 public class InverseOfA {
-    void InverseOfA(){
+    void InverseOfA() {
         Scanner in = new Scanner(System.in);
-        System.out.println("enter the rows and column of the matrix");
-        int m= in.nextInt(), n= in.nextInt();
-determinants deter = new determinants();
-deter.deter();
-        System.out.println(deter);
+        System.out.println("enter the number(must be a square matrix) of the rows and column (upto 3*3) :");
 
-    }
-}
+        int m = in.nextInt();
+        int n = in.nextInt();
+        if ((m > 0 && n > 0) && (m < 4 && n < 4)) {
+            int[][] first = new int[m][n];
+
+            System.out.println("enter the elements of the  matrix");
+
+            for (int c = 0; c < m; c++)
+                for (int d = 0; d < n; d++)
+                    first[c][d] = in.nextInt();
+
+            if (m < 2 && n < 2) {
+                System.out.println("determinant is :" + first[0][0]);
+
+            } else if ((m < 3 && n < 3) && (m > 1 && n > 1)) {
+                int c = 0, d = 0;
+                int deter = first[c][d] * first[c + 1][d + 1] - first[c + 1][d] * first[c][d + 1];
+                System.out.println("determinant is: " + deter);
+            } else if (m > 2 && n > 2) {
+
+                int c = 0, d = 0;
+                int x = first[c][d] * ((first[c + 1][d + 1] * first[c + 2][d + 2]) - (first[c + 2][d + 1] * first[c + 1][d + 2]));
+                int y = first[c][d + 1] * ((first[c + 1][d] * first[c + 2][d + 2]) - (first[c + 2][d] * first[c + 1][d + 2]));
+                int z = first[c][d + 2] * ((first[c + 1][d] * first[c + 2][d + 1]) - (first[c + 2][d] * first[c + 1][d + 1]));
+                int deter = x - y + z;
+                System.out.println("determinant is :" + deter);
+            }
+        }
+    }}
